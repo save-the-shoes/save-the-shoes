80e2dfd201402367cb50ea486e2772fd9d9b6425
Nick Johnstone <ncwjohnstone@gmail.com>
2015-09-17 11:36:27 +1200

    Add a car picker component

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
-  StyleSheet,
+  PickerIOS,
   Text,
   View,
 } = React;
 
+var PickerItemIOS = PickerIOS.Item;
+
+var CAR_MAKES_AND_MODELS = {
+  amc: {
+    name: 'AMC',
+    models: ['AMX', 'Concord', 'Eagle', 'Gremlin', 'Matador', 'Pacer'],
+  },
+  alfa: {
+    name: 'Alfa-Romeo',
+    models: ['159', '4C', 'Alfasud', 'Brera', 'GTV6', 'Giulia', 'MiTo', 'Spider'],
+  },
+  aston: {
+    name: 'Aston Martin',
+    models: ['DB5', 'DB9', 'DBS', 'Rapide', 'Vanquish', 'Vantage'],
+  },
+  audi: {
+    name: 'Audi',
+    models: ['90', '4000', '5000', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q5', 'Q7'],
+  },
+  austin: {
+    name: 'Austin',
+    models: ['America', 'Maestro', 'Maxi', 'Mini', 'Montego', 'Princess'],
+  },
+  borgward: {
+    name: 'Borgward',
+    models: ['Hansa', 'Isabella', 'P100'],
+  },
+  buick: {
+    name: 'Buick',
+    models: ['Electra', 'LaCrosse', 'LeSabre', 'Park Avenue', 'Regal',
+             'Roadmaster', 'Skylark'],
+  },
+  cadillac: {
+    name: 'Cadillac',
+    models: ['Catera', 'Cimarron', 'Eldorado', 'Fleetwood', 'Sedan de Ville'],
+  },
+  chevrolet: {
+    name: 'Chevrolet',
+    models: ['Astro', 'Aveo', 'Bel Air', 'Captiva', 'Cavalier', 'Chevelle',
+             'Corvair', 'Corvette', 'Cruze', 'Nova', 'SS', 'Vega', 'Volt'],
+  },
+};
+
 var SaveTheShoes = React.createClass({
+  getInitialState: function() {
+    return {
+      carMake: 'cadillac',
+      modelIndex: 3,
+    };
+  },
+
   render: function() {
+    var make = CAR_MAKES_AND_MODELS[this.state.carMake];
+    var selectionString = make.name + ' ' + make.models[this.state.modelIndex];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get farted, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Text></Text>
+        <Text>Enter your tank BA</Text>
+        <PickerIOS
+          selectedValue={this.state.carMake}
+          onValueChange={(carMake) => this.setState({carMake, modelIndex: 0})}>
+          {Object.keys(CAR_MAKES_AND_MODELS).map((carMake) => (
+            <PickerItemIOS
+              key={carMake}
+              value={carMake}
+              label={CAR_MAKES_AND_MODELS[carMake].name}
+              />
+            )
+          )}
+        </PickerIOS>
+        <Text>You selected: {selectionString}</Text>
       </View>
     );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
